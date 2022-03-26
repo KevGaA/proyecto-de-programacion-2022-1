585f8f6a76d135f35adfb3aad98238473b134ef9
Kevin Gallardo <kevingallardo904@gmail.com>
2022-03-26 19:30:47 -0300

    se desarrolló la ventana principal

--- ventana.py
+++ ventana.py
@@ -1,7 +1,23 @@
 from tkinter import *
+import tkinter as tk
+from tkinter import font
 
-app = Tk()
-app.geometry("800x600") ##dimensiones de la ventanan principal ANCHOX
+app = tk.Tk()
+app.geometry("800x600") ##dimensiones de la ventanan principal ANCHOXALTO
+app.configure(background="light blue")
+#app.title("graficador de expresiones matematicas")
+tk.Wm.wm_title(app, "graficador de expresiones matematicas")
+
+tk.Button(
+    app,
+    text="click, me",
+    font=("courier", 14),
+    bg="#00a8e8",
+    fg="white",
+).pack(
+    fill=tk.BOTH, 
+    expand=True,
+)
 
 
 
