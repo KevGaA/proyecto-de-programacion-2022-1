e5cfb49311e546ba3e02b16861a4441b2f389229
Kevin Gallardo <kevingallardo904@gmail.com>
2022-03-27 13:31:20 -0300

    se ajustaron los botones

--- ventana.py
+++ ventana.py
@@ -14,33 +14,29 @@ display = Entry(app)
 display.grid(row=1, columnspan=6, sticky=W+E)
 
 #botones de los numeros 
-Button(app, text="C").grid(row=2,column=0)
-Button(app, text="").grid(row=2,column=1)
-Button(app, text="").grid(row=2,column=2)
-Button(app, text="/").grid(row=2,column=3)
+Button(app, text="C").grid(row=2,column=0, sticky=W+E)
+Button(app, text="").grid(row=2,column=1, sticky=W+E)
+Button(app, text="").grid(row=2,column=2, sticky=W+E)
+Button(app, text="/").grid(row=2,column=3, sticky=W+E)
 
-Button(app, text="7").grid(row=3,column=0)
-Button(app, text="8").grid(row=3,column=1)
-Button(app, text="9").grid(row=3,column=2)
-Button(app, text="X").grid(row=3,column=3)
+Button(app, text="7").grid(row=3,column=0, sticky=W+E)
+Button(app, text="8").grid(row=3,column=1, sticky=W+E)
+Button(app, text="9").grid(row=3,column=2, sticky=W+E)
+Button(app, text="X").grid(row=3,column=3, sticky=W+E)
 
-Button(app, text="4").grid(row=4,column=0)
-Button(app, text="5").grid(row=4,column=1)
-Button(app, text="6").grid(row=4,column=2)
-Button(app, text="-").grid(row=4,column=3)
+Button(app, text="4").grid(row=4,column=0, sticky=W+E)
+Button(app, text="5").grid(row=4,column=1, sticky=W+E)
+Button(app, text="6").grid(row=4,column=2, sticky=W+E)
+Button(app, text="-").grid(row=4,column=3, sticky=W+E)
 
-Button(app, text="1").grid(row=5,column=0)
-Button(app, text="2").grid(row=5,column=1)
-Button(app, text="3").grid(row=5,column=2)
-Button(app, text="+").grid(row=5,column=3)
+Button(app, text="1").grid(row=5,column=0, sticky=W+E)
+Button(app, text="2").grid(row=5,column=1, sticky=W+E)
+Button(app, text="3").grid(row=5,column=2, sticky=W+E)
+Button(app, text="+").grid(row=5,column=3, sticky=W+E)
 
 ##BOTONES OPERADORES    
-Button(app, text="0").grid(row=6,column=0)
-Button(app, text=".").grid(row=6,column=1)
-Button(app, text="=").grid(row=6,column=3)
-
-
-
-
+Button(app, text="0").grid(row=6,columnspan=2, sticky=W+E)
+Button(app, text=".").grid(row=6,column=2, sticky=W+E)
+Button(app, text="=").grid(row=6,column=3, sticky=W+E)
 
 app.mainloop()
