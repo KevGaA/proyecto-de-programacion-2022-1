a5b0eeccbee461c7125613ff30da1bab4b5c5d4e
Kevin Gallardo <kevingallardo904@gmail.com>
2022-03-28 13:51:48 -0300

    se creo una nueva entrada de texto

--- ventana.py
+++ ventana.py
@@ -1,42 +1,60 @@
 from cgitb import text
+from ctypes import sizeof
+from msilib.schema import TextStyle
+from struct import pack
 from tkinter import *
 import tkinter as tk
 from tkinter import font
 
+
+i=0
+
+def numeros(n):
+    global i 
+    display.insert(i,n)
+    i+=1
+
 ##configuracion de la ventana 
 app = Tk()
-app.title("graficador de expresiones matematicas")
+app.title("Graficador de expresiones matematicas")
 app.geometry("800x600") ##dimensiones de la ventanan principal ANCHOXALTO
 app.configure(background="light blue")
 
 #entrada de la calculadora
 display = Entry(app)
-display.grid(row=1, columnspan=6, sticky=W+E)
+display.place(x=400,y=25,width=375,height=550)
+display.config(font=('Bradley hand ITC',20))
+
+#entrada de teclado
+d2 = Entry(app)
+d2.place(x=10,y=25,width=375,height=30)
+d2.config(font=('ARIAL',10))
 
 #botones de los numeros 
-Button(app, text="C").grid(row=2,column=0, sticky=W+E)
-Button(app, text="").grid(row=2,column=1, sticky=W+E)
-Button(app, text="").grid(row=2,column=2, sticky=W+E)
-Button(app, text="/").grid(row=2,column=3, sticky=W+E)
+'''
+btn0 = Button(app, text="C",height=5,width=10).place(x=25,y=25)
+btn1 = Button(app, text="",height=5,width=10).place(x=110,y=25)
+btn2 = Button(app, text="",height=5,width=10).place(x=195,y=25)
+btn3 = Button(app, text="/",height=5,width=10).place(x=280,y=25)
 
-Button(app, text="7").grid(row=3,column=0, sticky=W+E)
-Button(app, text="8").grid(row=3,column=1, sticky=W+E)
-Button(app, text="9").grid(row=3,column=2, sticky=W+E)
-Button(app, text="X").grid(row=3,column=3, sticky=W+E)
+btn4 = Button(app, text="7",height=5,width=10,command=lambda:numeros(7)).place(x=25,y=120)
+btn5 = Button(app, text="8",height=5,width=10,command=lambda:numeros(8)).place(x=110,y=120)
+btn6 = Button(app, text="9",height=5,width=10,command=lambda:numeros(9)).place(x=195,y=120)
+btn7 = Button(app, text="X",height=5,width=10).place(x=280,y=120)
 
-Button(app, text="4").grid(row=4,column=0, sticky=W+E)
-Button(app, text="5").grid(row=4,column=1, sticky=W+E)
-Button(app, text="6").grid(row=4,column=2, sticky=W+E)
-Button(app, text="-").grid(row=4,column=3, sticky=W+E)
+btn8 = Button(app, text="4",height=5,width=10,command=lambda:numeros(4)).place(x=25,y=215)
+btn9 = Button(app, text="5",height=5,width=10,command=lambda:numeros(5)).place(x=110,y=215)
+btn10 = Button(app, text="6",height=5,width=10,command=lambda:numeros(6)).place(x=195,y=215)
+btn11 = Button(app, text="-",height=5,width=10).place(x=280,y=215)
 
-Button(app, text="1").grid(row=5,column=0, sticky=W+E)
-Button(app, text="2").grid(row=5,column=1, sticky=W+E)
-Button(app, text="3").grid(row=5,column=2, sticky=W+E)
-Button(app, text="+").grid(row=5,column=3, sticky=W+E)
+btn12 = Button(app, text="1",height=5,width=10,command=lambda:numeros(1)).place(x=25,y=310)
+btn13 = Button(app, text="2",height=5,width=10,command=lambda:numeros(2)).place(x=110,y=310)
+btn14 = Button(app, text="3",height=5,width=10,command=lambda:numeros(3)).place(x=195,y=310)
+btn15 = Button(app, text="+",height=5,width=10).place(x=280,y=310)
 
 ##BOTONES OPERADORES    
-Button(app, text="0").grid(row=6,columnspan=2, sticky=W+E)
-Button(app, text=".").grid(row=6,column=2, sticky=W+E)
-Button(app, text="=").grid(row=6,column=3, sticky=W+E)
-
+Button(app, text="0",height=5,width=10,command=lambda:numeros(0)).place(x=25,y=405,width=165)
+Button(app, text=".",height=5,width=10).place(x=195,y=405)
+Button(app, text="=",height=5,width=10).place(x=280,y=405)
+'''
 app.mainloop()
