6d59b55b3bda75d1bb86e46674340ae573efc1e8
Kevin Gallardo <kevingallardo904@gmail.com>
2022-04-16 03:20:15 -0400

    se comentó el código

--- ventana.py
+++ ventana.py
@@ -34,6 +34,7 @@ def numeros(n):
         display.create_line(x1,y2,x1,y3)
         display.create_line(x1,y3,x2,y3)
     
+    #operadores 
     elif (n=="/"):
         display.create_line(20,10,10,30)
         display.addtag_below
